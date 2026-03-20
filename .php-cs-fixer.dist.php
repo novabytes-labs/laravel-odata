7da1fe05640ad56a20f28193d2a409e78c4026bd
Stefan <stefan@nimiq.com>
2026-03-20 15:17:31 +0100

    CI: setup

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+$finder = PhpCsFixer\Finder::create()
+    ->in([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ]);
+
+return (new PhpCsFixer\Config())
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@PER-CS' => true,
+        'strict_param' => true,
+        'declare_strict_types' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'no_unused_imports' => true,
+        'ordered_imports' => ['sort_algorithm' => 'alpha'],
+        'single_quote' => true,
+        'trailing_comma_in_multiline' => ['elements' => ['arguments', 'arrays', 'parameters']],
+        'no_whitespace_in_blank_line' => true,
+        'blank_line_before_statement' => ['statements' => ['return']],
+    ])
+    ->setFinder($finder);
